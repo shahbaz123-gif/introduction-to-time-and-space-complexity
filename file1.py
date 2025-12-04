d7d2131883755ba9cb4391714caa23cf176c257c
shahbaz123-gif <sajjadahmadjan30@gmail.com>
2025-12-04 21:42:04 +0500

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,37 @@
+def fun1(n):
+
+    return n*(n+1)/2
+
+print(fun1(4))
+
+
+def fun2(n):
+
+    sum=0
+
+    for i in range(1,n+1):
+
+        sum+=i
+
+    return sum
+
+print(fun2(4))
+
+# for function2 the algorithm goes like:
+# 1 + 2 +3 + 4
+
+#so, the number of iterations will be 1+1+1+1=4
+
+def fun3(n):
+
+    sum=0
+
+    for i in range(1,n+1):
+        
+        for j in range(1,i+1):
+
+            sum+=1
+
+    return sum
+
+print(fun3(4))        
---
+++ file2.py
@@ -0,0 +1,15 @@
+#for function 1, the algorithms goes like this:
+#                                              (4*5)/2
+#so the number of iterations will be 1.
+  
+
+
+#for function2 the algorithm goes like:
+#                                      1+2+3+4
+#so the number of iterations will be 1 + 1 + 1 + 1=4
+
+
+
+#for function3 the algorithms goes like:
+#                       1 + (1+1) + (1+1+1) + (1+1+1+1)
+#so the number of iterations will be 1 + 2 + 3 + 4=
